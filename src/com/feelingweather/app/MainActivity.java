9eaa8afd1f06e3acf1a469df84a658c898dd27c5
Administrator <Administrator@PC-20160305WMAO>
2016-03-12 15:28:48 +0800

    test

---
+++ src/com/feelingweather/app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.feelingweather.app;
+
+import android.support.v7.app.ActionBarActivity;
+
+import com.example.feelingweather.R;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
